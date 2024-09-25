90c366446e8abbec01c896c402e70c99fedcd969
Manas Bisht <manasbisht1142004@gmail.com>
2024-09-26 02:41:17 +0530

    Changed app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template,request, jsonify
 import joblib
 import os
 from model import KerasClassifier, TextPreprocessor
-import logging
-logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
@@ -18,24 +16,18 @@ def predict():
    
     data = request.get_json()
     text = data.get('inputText')
+    
+    if text != None:
+        print(os.path.join(os.path.dirname(__file__), 'Models', 'Preprocessing.joblib'))
+        preprocess = joblib.load(os.path.join(os.path.dirname(__file__), 'Models', 'Preprocessing.joblib'))
+        model = joblib.load(os.path.join(os.path.dirname(__file__), 'Models', 'spam-ham-pipe.joblib'))
 
-    try:
-        if text != None:
-            print(os.path.join(os.path.dirname(__file__), 'Models', 'Preprocessing.joblib'))
-            preprocess = joblib.load(os.path.join(os.path.dirname(__file__), 'Models', 'Preprocessing.joblib'))
-            model = joblib.load(os.path.join(os.path.dirname(__file__), 'Models', 'spam-ham-pipe.joblib'))
+        ans = model.predict(preprocess.transform([text]))
 
-            ans = model.predict(preprocess.transform([text]))
+        ans = ans[0]
 
-            ans = ans[0]
-
-        else:
-            ans=""
-    except:
-        ans = f"dir: {os.getcwd}, sys: {sys.path}"
-        logging.info(f"Current directory: {os.getcwd()}")
-        import sys
-        logging.info(f"sys.path: {sys.path}")
+    else:
+        ans=""
 
     # return render_template("index.html", answer=ans)
     return jsonify({'answer': ans}) 
