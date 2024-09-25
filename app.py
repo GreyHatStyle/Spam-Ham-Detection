f9dbb51c07aaa9aa9f817b3d3b6baa063774ebc1
Manas Bisht <manasbisht1142004@gmail.com>
2024-09-26 01:32:56 +0530

    Changed app.py removed if name == __main__

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ def hello():
 
 @app.route("/predict", methods=['POST', 'GET'])
 def predict():
-    
+    from custom_class import KerasClassifier, TextPreprocessor
     data = request.get_json()
     text = data.get('inputText')
 
     if text != None:
-        print(os.path.join(os.path.dirname(__file__), 'Models', 'Preprocessing.joblib'))
+        print(f"Address==========================:{os.path.join(os.path.dirname(__file__), 'Models', 'Preprocessing.joblib')}")
         preprocess = joblib.load(os.path.join(os.path.dirname(__file__), 'Models', 'Preprocessing.joblib'))
         model = joblib.load(os.path.join(os.path.dirname(__file__), 'Models', 'spam-ham-pipe.joblib'))
 
@@ -34,6 +34,6 @@ def predict():
     # return render_template("index.html", answer=ans)
     return jsonify({'answer': ans}) 
 
-if __name__ == "__main__":
-    from custom_class import KerasClassifier, TextPreprocessor
-    app.run(debug=True)
+
+from custom_class import KerasClassifier, TextPreprocessor
+app.run(debug=True)
